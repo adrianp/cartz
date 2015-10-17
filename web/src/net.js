583b69c040bda2e77456f926084ed0c0104de1c7
Lucian Parfeni <lucian.parfeni@cynny.com>
2015-10-17 14:49:16 +0300

    net

---
+++ web/src/net.js
@@ -0,0 +1,18 @@
+const host = 'http://192.168.2.121:8080';
+
+const newGame = (gameId) => {
+	fetch(host + '/api/newgame', {
+		method: 'post',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			id: gameId
+		})
+	}).then((resp) => {
+		console.log('resp', resp);
+	});
+};
+
+module.exports.newGame = newGame;
